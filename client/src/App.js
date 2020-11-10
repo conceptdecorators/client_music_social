cdc1442813c417c1c64979eff6218f43a22251a0
Concept decorator <conceptdecorators@email.com>
2020-11-10 00:50:32 -0500

    Private route for dash

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -23,22 +25,24 @@ const App = () => {
   }, []);
 
   return (
-<Provider store={store}>
-  <Router>
-    <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-  </Router>
-</Provider>
-)};
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+    </Router>
+  </Provider>
+  );
+};
 
 export default App;
     
