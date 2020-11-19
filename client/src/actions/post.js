5e82dd6339ee43f659d1db3324835acadd48a5d1
Concept decorator <conceptdecorators@email.com>
2020-11-19 18:24:14 -0500

    components/layout/Landing.js propType not capital letters

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -10,7 +10,7 @@ export const getPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
-    })
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
